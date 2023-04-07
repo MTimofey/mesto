7f550f4e28c2fffad332a6976afaaff155c75cac
TimofeyM <tmorozov@lintech.ru>
2023-04-07 13:44:14 +0300

    Fixed(cards.js/script.js): changed photos/pictures and recorrect srcipt and MarkDown

--- script/cards.js
+++ script/cards.js
@@ -24,27 +24,27 @@ const initialCards = [
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   },
   {
-    name: 'Тетрагидроканнабинол',
+    name: 'Какой-то мужик',
     link: 'https://images.unsplash.com/photo-1680738498389-55af103d8eab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80'
   }, 
   {
-    name: 'Джонни Депп',
-    link: 'https://ltdfoto.ru/images/2023/04/07/171455-zhivopis-illustracia-art-voda-oblako-500x.jpg'
-  },
-  {
-    name: 'Джон Колтрейн',
-    link: 'https://unsplash.com/photos/qXjINparwW0'
+    name: 'Умиротворение',
+    link: 'https://images.unsplash.com/photo-1680582742810-164f11a8cefa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80'
   },
   {
     name: 'Отдых на пляже',
-    link: 'https://unsplash.com/photos/spFn2ilD1aw'
+    link: 'https://images.unsplash.com/photo-1680553362060-9ed111958ada?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80'
   },
   {
-    name: 'Пейзаж',
-    link: 'https://unsplash.com/photos/TBW2aebtz0E'
+    name: 'Рука-телефон',
+    link: 'https://images.unsplash.com/photo-1680462155145-0152dc0646dc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80'
+  },
+  {
+    name: 'Собор в Дании',
+    link: 'https://images.unsplash.com/photo-1680742065565-3bdca8ac6dcd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80'
   },
   {
     name: 'Китайский фонарь',
-    link: 'https://unsplash.com/photos/JzPg38m570c'
+    link: 'https://images.unsplash.com/photo-1680695917815-6431604e1d3d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80'
   },
 ];
